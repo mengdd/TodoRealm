9872bff40f52a18d9bfe55a6ec07c8718b6d0701
Dandan MENG <mengdandanno1@163.com>
2017-02-08 15:35:08 +0800

    set default realm configuration

--- app/src/main/java/com/ddmeng/todorealm/TodoApplication.java
+++ app/src/main/java/com/ddmeng/todorealm/TodoApplication.java
@@ -6,12 +6,22 @@ import com.facebook.stetho.Stetho;
 import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
 
 import io.realm.Realm;
+import io.realm.RealmConfiguration;
 
 public class TodoApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         Realm.init(this);
+
+        RealmConfiguration config = new RealmConfiguration.Builder()
+                .name("todo.realm")
+                .schemaVersion(1)
+                .deleteRealmIfMigrationNeeded()
+                .build();
+
+        Realm.setDefaultConfiguration(config);
+
         if (BuildConfig.DEBUG) {
             Stetho.initialize(
                     Stetho.newInitializerBuilder(this)
