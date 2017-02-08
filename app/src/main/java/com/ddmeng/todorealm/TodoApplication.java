9045917f5e2cdb842b4a789f590205063d6c09b2
Dandan MENG <mengdandanno1@163.com>
2017-02-08 11:48:44 +0800

    add stetho-realm

--- app/src/main/java/com/ddmeng/todorealm/TodoApplication.java
+++ app/src/main/java/com/ddmeng/todorealm/TodoApplication.java
@@ -2,6 +2,9 @@ package com.ddmeng.todorealm;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
 import io.realm.Realm;
 
 public class TodoApplication extends Application {
@@ -9,5 +12,12 @@ public class TodoApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Realm.init(this);
+        if (BuildConfig.DEBUG) {
+            Stetho.initialize(
+                    Stetho.newInitializerBuilder(this)
+                            .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                            .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                            .build());
+        }
     }
 }
